bcfb1c6d87964469ebd664a4dd3be9b1e91acd5e
hellozjf <908686171@qq.com>
2018-10-31 10:35:19 +0800

    服务端还需要从数据库中启动

--- ssserver/src/main/java/com/hellozjf/shadowsocks/ssserver/SSServer.java
+++ ssserver/src/main/java/com/hellozjf/shadowsocks/ssserver/SSServer.java
@@ -45,46 +45,24 @@ public class SSServer {
         userInfoRepository.save(userInfo);
     }
 
-    public void start() throws Exception {
-
-        // 首先从数据库中获取用户信息
-        List<UserInfo> userInfoList = userInfoRepository.findAll();
-        if (userInfoList.size() == 0) {
-            // 说明数据库中没有用户信息，那么就从classpath:config.json中初始化用户
-            if (config.getPortPassword() != null) {
-                // 如果当前配置中有多用户，那么以多用户为准
-                for (Map.Entry<Integer, String> portPassword : config.getPortPassword().entrySet()) {
-                    try {
-                        // 启动对应的TCP和UDP端口
-                        startSingle(config.getServer(),
-                                portPassword.getKey(),
-                                portPassword.getValue(),
-                                config.getMethod());
-
-                        // 添加user+port用户
-                        addUser(portPassword.getKey(),
-                                "user" + portPassword.getKey(),
-                                portPassword.getValue(),
-                                config.getTimeout(),
-                                config.getMethod());
-                    } catch (Exception e) {
-                        // TODO 如果端口开启成功，但是数据库写入失败，怎么办？
-                        log.error("startSingle failed, e = {}", e);
-                    }
-                }
-            } else {
-                // 否则以单用户为准
+    /**
+     * 通过配置文件启动程序，同时初始化数据库
+     */
+    private void startWithConfigAndInitUserInfo() {
+        if (config.getPortPassword() != null) {
+            // 如果当前配置中有多用户，那么以多用户为准
+            for (Map.Entry<Integer, String> portPassword : config.getPortPassword().entrySet()) {
                 try {
                     // 启动对应的TCP和UDP端口
                     startSingle(config.getServer(),
-                            config.getServerPort(),
-                            config.getPassword(),
+                            portPassword.getKey(),
+                            portPassword.getValue(),
                             config.getMethod());
 
-                    // 添加admin用户
-                    addUser(config.getServerPort(),
-                            "admin",
-                            config.getPassword(),
+                    // 添加user+port用户
+                    addUser(portPassword.getKey(),
+                            "user" + portPassword.getKey(),
+                            portPassword.getValue(),
                             config.getTimeout(),
                             config.getMethod());
                 } catch (Exception e) {
@@ -92,8 +70,57 @@ public class SSServer {
                     log.error("startSingle failed, e = {}", e);
                 }
             }
-        }
+        } else {
+            // 否则以单用户为准
+            try {
+                // 启动对应的TCP和UDP端口
+                startSingle(config.getServer(),
+                        config.getServerPort(),
+                        config.getPassword(),
+                        config.getMethod());
 
+                // 添加admin用户
+                addUser(config.getServerPort(),
+                        "admin",
+                        config.getPassword(),
+                        config.getTimeout(),
+                        config.getMethod());
+            } catch (Exception e) {
+                // TODO 如果端口开启成功，但是数据库写入失败，怎么办？
+                log.error("startSingle failed, e = {}", e);
+            }
+        }
+    }
+
+    /**
+     * 通过数据库的内容启动程序
+     */
+    private void startWithDatabase(List<UserInfo> userInfoList) {
+        for (UserInfo userInfo : userInfoList) {
+            String server = config.getServer();
+            Integer port = userInfo.getPort();
+            String password = userInfo.getPassword();
+            String method = userInfo.getMethod();
+            try {
+                startSingle(server, port, password, method);
+            } catch (Exception e) {
+                // TODO 这是个严重的错误，端口启动失败，用户就会无法使用这个服务，那么应该要将占用该端口的进程杀掉，再次尝试启动端口
+                log.error("启动端口{}失败", port);
+            }
+        }
+    }
+
+    public void start() throws Exception {
+
+        // 首先从数据库中获取用户信息
+        List<UserInfo> userInfoList = userInfoRepository.findAll();
+        if (userInfoList.size() == 0) {
+            // 说明数据库中没有用户信息，那么就从classpath:config.json中初始化用户
+            startWithConfigAndInitUserInfo();
+        } else {
+            // 说明数据库中有用户信息，那么就从数据库中初始化端口
+            startWithDatabase(userInfoList);
+        }
     }
 
     private void startSingle(String server, Integer port, String password, String method) throws Exception {
