f8a289c9ec659b6410b9ec5a19e6cbe7f1ecf1a3
hellozjf <908686171@qq.com>
2018-11-05 09:06:59 +0800

    版本1.0.7，端口范围更改为10000-10101，同时允许创建用户的时候分配端口，使用docker进行发布

--- ssserver/src/main/java/com/hellozjf/shadowsocks/ssserver/service/impl/UserInfoServiceImpl.java
+++ ssserver/src/main/java/com/hellozjf/shadowsocks/ssserver/service/impl/UserInfoServiceImpl.java
@@ -9,6 +9,7 @@ import com.hellozjf.shadowsocks.ssserver.exception.ShadowsocksException;
 import com.hellozjf.shadowsocks.ssserver.repository.UserInfoRepository;
 import com.hellozjf.shadowsocks.ssserver.service.IUserInfoService;
 import com.hellozjf.shadowsocks.ssserver.util.ChannelUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,7 @@ import java.util.List;
  * @author Jingfeng Zhou
  */
 @Service
+@Slf4j
 public class UserInfoServiceImpl implements IUserInfoService {
 
     @Autowired
@@ -50,8 +52,6 @@ public class UserInfoServiceImpl implements IUserInfoService {
     @Override
     public UserInfo save(UserInfo userInfo) {
 
-        // TODO 新增用户后，新增的端口无法使用，这是个bug！
-
         // 检查username、email、phone是否有重复
         checkDuplicateUsernameEmailPhone(userInfo);
 
@@ -59,7 +59,11 @@ public class UserInfoServiceImpl implements IUserInfoService {
         checkNullMethodTimeout(userInfo);
 
         // 分配一个新的port，并启动它
-        allocServerPort(userInfo);
+        if (userInfo.getPort() == null) {
+            allocServerPort(userInfo);
+        } else {
+            allocServerPort(userInfo, userInfo.getPort());
+        }
 
         // 新增用户
         userInfo.setId(null);
@@ -78,6 +82,7 @@ public class UserInfoServiceImpl implements IUserInfoService {
         // 删除用户的时候，要将该用户对应的端口也关闭
         SSServer.TcpAndUdpChannel tcpAndUdpChannel = ssServer.getPortChannelMap().get(userInfo.getPort());
         ChannelUtils.closeChannels(tcpAndUdpChannel.getTcpChannel(), tcpAndUdpChannel.getUdpChannel());
+        // TODO 删除用户的时候，要将FlowStatisticsDetail和FlowSummary表里面的数据也要删除
         userInfoRepository.deleteById(id);
     }
 
@@ -97,7 +102,7 @@ public class UserInfoServiceImpl implements IUserInfoService {
 
         // TODO 检查是否修改了method和timeout，如果修改了也要重新启动端口
 
-        // TODO 检查是否修改了端口，如果修改了端口要重新分配端口，现在先不允许修改端口
+        // TODO 检查是否修改了端口，如果修改了端口要重新分配端口，端口只允许分配在10000-10101，修改了端口还要修改FlowStatisticsDetail和FlowSummary表
         userInfo.setPort(null);
 
         // 修改用户
@@ -106,29 +111,57 @@ public class UserInfoServiceImpl implements IUserInfoService {
         return result;
     }
 
+    /**
+     * 让系统随机分配一个端口
+     * @param userInfo
+     */
     private void allocServerPort(UserInfo userInfo) {
 
         // 先查找所有已经使用的端口
         List<UserInfo> userInfoList = userInfoRepository.findAll();
-        List<Integer> allocedPorts = new ArrayList<>();
-        for (UserInfo user : userInfoList) {
-            allocedPorts.add(user.getPort());
-        }
+        List<Integer> allocedPorts = findAllAllocedPorts(userInfoList);
 
         // 然后从minPort号端口开始分配，直到找到一个有效的端口或者超过maxPort为止
-        userInfo.setPort(-1);
-        for (int port = customConfig.getMinPort(); port <= customConfig.getMaxPort(); port++) {
-            if (allocedPorts.indexOf(port) == -1) {
-                // 试试看能不能分配端口
+        allocPort(userInfo, allocedPorts);
+
+        // 说明端口没有分配成功，那么该用户不可能再分配到端口了，只能返回失败
+        if (userInfo.getPort().intValue() == -1) {
+            throw new ShadowsocksException(ResultEnum.CAN_NOT_ALLOC_SS_SERVER_PORT);
+        }
+    }
+
+    /**
+     * 优先使用priorPort，如果priorPort被占用，再由系统分配端口
+     * @param userInfo
+     * @param priorPort
+     */
+    private void allocServerPort(UserInfo userInfo, Integer priorPort) {
+
+        // 先查找所有已经使用的端口
+        List<UserInfo> userInfoList = userInfoRepository.findAll();
+        List<Integer> allocedPorts = findAllAllocedPorts(userInfoList);
+
+        // 如果priorPort不在allocedPorts里面，而且用该端口开启ssserver成功，那么就算成功
+        boolean bAllocSuccessed = false;
+        if (allocedPorts.indexOf(priorPort) == -1) {
+            if (priorPort.intValue() < customConfig.getMinPort().intValue()  ||
+                    priorPort.intValue() > customConfig.getMaxPort().intValue()) {
+                log.debug("自定义端口{}超出范围{}-{}", priorPort, customConfig.getMinPort(), customConfig.getMaxPort());
+            } else {
                 try {
-                    ssServer.startSingle(jsonConfig.getServer(), port, userInfo.getPassword(), userInfo.getMethod());
-                    // 端口开启成功，那就说明这是我们需要的端口了
-                    userInfo.setPort(port);
-                    break;
+                    ssServer.startSingle(jsonConfig.getServer(), priorPort, userInfo.getPassword(), userInfo.getMethod());
+                    bAllocSuccessed = true;
                 } catch (Exception e) {
-                    continue;
+                    log.debug("开启自定义端口{}失败", priorPort);
                 }
             }
+        } else {
+            log.debug("自定义端口{}已被占用", priorPort);
+        }
+
+        // 如果没有分配成功，那么从minPort号端口开始分配，直到找到一个有效的端口或者超过maxPort为止
+        if (! bAllocSuccessed) {
+            allocPort(userInfo, allocedPorts);
         }
 
         // 说明端口没有分配成功，那么该用户不可能再分配到端口了，只能返回失败
@@ -137,6 +170,23 @@ public class UserInfoServiceImpl implements IUserInfoService {
         }
     }
 
+    /**
+     * 找出所有已经被分配的端口号
+     * @param userInfoList
+     * @return
+     */
+    private List<Integer> findAllAllocedPorts(List<UserInfo> userInfoList) {
+        List<Integer> allocedPorts = new ArrayList<>();
+        for (UserInfo user : userInfoList) {
+            allocedPorts.add(user.getPort());
+        }
+        return allocedPorts;
+    }
+
+    /**
+     * 检测method和timeout字段是否为null
+     * @param userInfo
+     */
     private void checkNullMethodTimeout(UserInfo userInfo) {
         // TODO 讲道理，这里需要检查method字段，确保用户不会填了一个无效的method
         if (userInfo.getMethod() == null) {
@@ -147,6 +197,10 @@ public class UserInfoServiceImpl implements IUserInfoService {
         }
     }
 
+    /**
+     * 检查数据库中用户名、邮箱、手机号码是否重复
+     * @param userInfo
+     */
     private void checkDuplicateUsernameEmailPhone(UserInfo userInfo) {
 
         if (userInfo.getUsername() != null && userInfoRepository.findByUsername(userInfo.getUsername()) != null) {
@@ -160,6 +214,28 @@ public class UserInfoServiceImpl implements IUserInfoService {
         if (userInfo.getPhone() != null && userInfoRepository.findByPhone(userInfo.getPhone()) != null) {
             // phone重复了
             throw new ShadowsocksException(ResultEnum.DUPLICATE_PHONE);
+        }
+    }
+
+    /**
+     * 根据已经分配的端口列表，以及配置文件中最大和最小端口号，开始分配端口
+     * @param userInfo
+     * @param allocedPorts
+     */
+    private void allocPort(UserInfo userInfo, List<Integer> allocedPorts) {
+        userInfo.setPort(-1);
+        for (int port = customConfig.getMinPort(); port <= customConfig.getMaxPort(); port++) {
+            if (allocedPorts.indexOf(port) == -1) {
+                // 试试看能不能分配端口
+                try {
+                    ssServer.startSingle(jsonConfig.getServer(), port, userInfo.getPassword(), userInfo.getMethod());
+                    // 端口开启成功，那就说明这是我们需要的端口了
+                    userInfo.setPort(port);
+                    break;
+                } catch (Exception e) {
+                    continue;
+                }
+            }
         }
     }
 }
